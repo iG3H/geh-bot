890432d4491ca55f186fed1e03f1f2ab343a2d07
Glitch (hello-express) <none>
2020-05-24 12:50:45 +0000

    👧🌵 Checkpoint ./index.js:16608023/1707 ./package.json:16608023/1912 ./config.json:16608023/17 ./watch.json:16608023/2

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Recebido");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000); // Código que deixa o Bot Online
+
+const Discord = require("discord.js"); //Conexão com a livraria Discord.js
+const client = new Discord.Client(); //Criação de um novo Client
+const config = require("./config.json"); //Pegando o prefixo do bot para respostas de comandos
+
+client.login(process.env.TOKEN); //Ligando o Bot caso ele consiga acessar o token
