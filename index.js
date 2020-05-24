b37297a8f63df49b3635a5af9b3169d1901e6e6e
Glitch (hello-express) <none>
2020-05-24 13:02:42 +0000

    📷🎯 Checkpoint ./index.js:16608023/702 ./commands/say.js:16608023/208

--- index.js
+++ index.js
@@ -14,4 +14,23 @@ const Discord = require("discord.js"); //Conexão com a livraria Discord.js
 const client = new Discord.Client(); //Criação de um novo Client
 const config = require("./config.json"); //Pegando o prefixo do bot para respostas de comandos
 
+client.on("message", async message => {
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+  if (!message.content.startsWith(config.prefix)) return;
+  if (message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)
+  ) return;
+
+  let args = message.content.split(" ").slice(1);
+  let command = message.content.split(" ")[0];
+  command = command.slice(config.prefix.length);
+  try {
+    let commandFile = require(`./commands/${command}.js`);
+    delete require.cache[require.resolve(`./commands/${command}.js`)];
+    return commandFile.run(client, message, args);
+  } catch (err) {
+    console.error("Erro:" + err);
+  }
+});
+
 client.login(process.env.TOKEN); //Ligando o Bot caso ele consiga acessar o token
