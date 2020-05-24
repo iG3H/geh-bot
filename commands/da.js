49cf27f754565ad2ab8d730708a80ec1844935a8
Glitch (hello-express) <none>
2020-05-24 18:20:33 +0000

    🛎🏯 Checkpoint ./commands/da.js:16608023/304 ./watch.json:16608023/14

--- commands/da.js
+++ commands/da.js
@@ -3,12 +3,12 @@ const Discord = require('discord.js');
 exports.run = async (client, message, args) => {
 
 var list = [
-    'Recebeu 1 de Cura :heart:',
-    'Recebeu 3 de Cura :sparkling_heart:',
-    'Recebeu 5 de Cura :heartpulse:',
-    'Recebeu 2 de Dano ',
-    'Recebeu 4 de Dano ',
-    'Recebeu 6 de Dano ' 
+    'RECEBEU 1 DE CURA - :heart:',
+    'RECEBEU 3 DE CURA - :sparkling_heart:',
+    'RECEBEU 5 DE CURA - :heartpulse:',
+    'RECEBEU 2 DE DANO - :knife:',
+    'RECEBEU 3 DE DANO - :axe:',
+    'RECEBEU 6 DE DANO - :boom:' 
 ];
 
 var rand = list[Math.floor(Math.random() * list.length)];
@@ -17,7 +17,7 @@ let user = message.mentions.users.first() || client.users.cache.get(args[0]);
 let avatar = message.author.displayAvatarURL({format: "png"});
   const embed = new Discord.MessageEmbed()
         .setTitle('O jogador selecionado:')
-        .setColor('#FFFFFF')
+        .setColor('#e9eff0')
         .setDescription(rand)
 
 await message.channel.send(embed);
