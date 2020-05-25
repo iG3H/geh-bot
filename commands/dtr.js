59f981b090b4feb9553c596cf1bc1ef4e241893a
Glitch (hello-express) <none>
2020-05-25 18:00:30 +0000

    🐂🌃 Checkpoint ./commands/dtr.js:16608023/3399 ./commands/del.js:16608023/480 ./watch.json:16608023/4

--- commands/dtr.js
+++ commands/dtr.js
@@ -4,11 +4,16 @@ exports.run = async (client, message, args) => {
   if (!message.member.permissions.has("MANAGE_MESSAGES"))
     return message.reply(
       "Tá querendo apagar o que? Você não tem permissão de Gerenciar Mensagens, FDP!"
-                        )
-
-  const channel = message.channel;
-  const fetchmessage = await channel.fetchmessage ();
-  await channel.bulkdelete(fetchmessage);
-  message.reply ("Destruí todas as Evidências!");
+    );
   
+  const fetched = await message.channel.messages.fetch({
+   
+  });
+  
+  message.channel.bulkDelete(fetched);
+  message.channel
+    .send(`Limpei ${args[0]} menssagens nesse caralho!`)
+    .catch(error =>
+      console.log(`Não consegui deletar as merdas das mensagens por causa de: ${error}`)
+    );
 };
