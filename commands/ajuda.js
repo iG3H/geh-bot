aa31351736c62b2620a26ea7224e2e8f3902f882
Glitch (hello-express) <none>
2020-05-25 20:58:56 +0000

    🚈🎏 Checkpoint ./commands/fale.js:16608023/426 ./commands/ajuda.js:16608023/13

--- commands/ajuda.js
+++ commands/ajuda.js
@@ -2,7 +2,8 @@ const Discord = require('discord.js');
 
 exports.run = async (client, message, args) => {
 
-const com = (`
+const com = ({
+  `
               COMANDO GERAIS"
               g.fale : O bot repete a menssagem escrita depois do comando.
               g.ajuda : Exibe a lista de comandos dis poniveis.
@@ -17,7 +18,7 @@ const com = (`
               COMANDO ADMINISTRATIVOS"
               g.delete <número entre 1 a 100> : Apaga a quantidade informada de menssagem no canal.
               g.destroi : Apaga uma quantidade variavél de menssagens no canal.
-  `)
+  `})
    
 const embed = new Discord.MessageEmbed()
         
--- commands/fale.js
+++ commands/fale.js
@@ -1,7 +1,14 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
+  
+  const embed = new Discord.MessageEmbed();
   const sayMessage = args.join(" ");
+  
   message.delete().catch(O_o => {});
+  
   message.channel.send(sayMessage);
+  
+  
+  
 };
