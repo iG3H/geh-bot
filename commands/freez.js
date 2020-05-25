66dd8feb7e98db295c6bccb32bb3ad22a81dc578
Glitch (hello-express) <none>
2020-05-25 14:31:39 +0000

    🌿🌾 Checkpoint ./commands/freez.js:16608023/1263 ./watch.json:16608023/1

---
+++ commands/freez.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+exports.run = async (client, message, args) => {
+
+var list = [
+  '',
+  ''
+   
+];
+
+var rand = list[Math.floor(Math.random() * list.length)];
+let user = message.mentions.users.first() || client.users.cache.get(args[0]);
+if (!user) {
+return message.reply('você mencionou errado um User, tente do jeito certo animal.');
+}
+
+let avatar = message.author.displayAvatarURL({format: "png"});
+  const embed = new Discord.MessageEmbed()
+        .setTitle('Segunda Habilidade: Infinitus Frigus')
+        .setColor('#74a1e8')
+        .setDescription(`:snowflake: ${message.author} CONGELOU ${user} :snowflake:`)
+        .setImage(rand)
+        .setTimestamp()
+        .setThumbnail(avatar)
+        .setFooter('Nada Escapa do Gelo Verdadeiro!')
+        .setAuthor(message.author.tag, avatar);
+  await message.channel.send(embed);
+  
+}
