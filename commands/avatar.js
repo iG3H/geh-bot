e802f8334105bd39e31f2399e4933fab0008c76f
Glitch (hello-express) <none>
2020-05-25 23:40:41 +0000

    🕋🐩 Checkpoint ./commands/avatar.js:16608023/102 ./watch.json:16608023/10

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 exports.run = async (client, message, args) => {
 
-  var list = [
+  var list1 = [
     
     'https://imgur.com/GkeMEmn.png',
     'https://imgur.com/DqZxAwR.png',
@@ -10,17 +10,19 @@ exports.run = async (client, message, args) => {
     'https://imgur.com/lEJElPF.png',
     'https://imgur.com/y3v4oVL.png'           
   ];
+   
+  let element = list1[Math.floor(Math.random() * list1.length)];
   
-  
-  let rand = list[Math.floor(Math.random() * list.length)];
   let avatar = message.author.displayAvatarURL({format: "png"});
   const embed = new Discord.MessageEmbed()
   
         .setTitle('Este é seu Avatar!')
         .setColor('#afb0b3')
         .setDescription('')
-        .setImage(rand)
-        .setThumbnail(avatar)          
+        .setImage(element)
+        .setImage(element)
+        .setThumbnail(avatar)
+  
   await message.channel.send(embed);
   
 }
