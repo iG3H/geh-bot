4ca5a9708e2cb7c93c8b217e94e130c1e963ee18
Glitch (hello-express) <none>
2020-05-25 15:16:43 +0000

    👋🛍 Checkpoint ./commands/maldite.js:16608023/55 ./watch.json:16608023/2

--- commands/maldite.js
+++ commands/maldite.js
@@ -18,12 +18,12 @@ return message.reply('você mencionou errado um User, tente do jeito certo anima
 let avatar = message.author.displayAvatarURL({format: "png"});
   const embed = new Discord.MessageEmbed()
         .setTitle('Terceira Habilidade: Tenebris Mortis')
-        .setColor('#74a1e8')
+        .setColor('#3a0166')
         .setDescription(`:skull: ${message.author} AMALDIÇOU ${user} :skull:`)
         .setImage(rand)
         .setTimestamp()
         .setThumbnail(avatar)
-        .setFooter('Trevas, comsumam toda a vida.')
+        .setFooter('As trevas consomen tudo! -Veigas Terr.')
         .setAuthor(message.author.tag, avatar);
   await message.channel.send(embed);
   
