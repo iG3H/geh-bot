a12267426d869153d8fc856ef90eb17b07721578
Glitch (hello-express) <none>
2020-05-25 19:59:42 +0000

    🌟🌛 Checkpoint ./commands/ajuda.js:16608023/671 ./watch.json:16608023/8

--- commands/ajuda.js
+++ commands/ajuda.js
@@ -1,27 +1,27 @@
 const Discord = require('discord.js');
-const Commands = require('commands/');
+const Commands = require('.commands/');
 
 exports.run = async (client, message, args) => {
     
-const descriptions = {};   
-  
-  var text = "Bot Feito por GêH \n";
+const descriptions = {
+                       "g.ajuda" : "Use esse comando para ver todos os comandos disponiveis!"
+};   
+    
+var text = "Bot Feito por GêH \n";
   Object.keys(Commands).ForEach(command =>{
-   texto += ``\n  
-    
-    
-    
+   text += `\n ${command}: ${descriptions[command] ? descriptions[command]  : 'Não tem Descrição'}`    
   }
   )
-  
+   
 const embed = new Discord.MessageEmbed()
         
         .setTitle('Estes são os comandos disponiveis atualmente:')
         .setColor('#f7f7f5')
-        .setDescription()
+        .setDescription(text)
         .setTimestamp()
         .setFooter('Esperamos aumentar a lista em breve! Obrigado pelo uso.')
         
 await message.channel.send(embed);
+  
   
 }
