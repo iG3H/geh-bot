2592ff87b809eaef861cea39441039b12fc6f423
Glitch (hello-express) <none>
2020-05-26 00:27:51 +0000

    🌮👶 Checkpoint ./commands/avatar.js:16608023/576 ./watch.json:16608023/6

--- commands/avatar.js
+++ commands/avatar.js
@@ -6,21 +6,41 @@ exports.run = async (client, message, args) => {
     
     'Homem',
     'Mulher',
-    'Indefinido'
+    'Indefinido',
+    'Desconhecido'
   ];
-  
   let sexrand = sex[Math.floor(Math.random() * sex.length)];
+  
+  var age = [
+    
+    '13 anos',
+    '20 anos',
+    '25 anos',
+    '31 anos',
+    '40 anos',
+    '47 anos',
+    '55 anos',
+    '62 anos',
+    '70 anos',
+    '88 anos',
+    '90 anos',
+    '111 anos',
+    'desconhecido'
+    
+  ];
+  let agerand = age[Math.floor(Math.random() * age.length)];
+  
   
 const avatarlist = [` 
 
 ${message.author}, seu avatar está pronto:
   
-. Sexo:
-. Idade:
+. Sexo: ${sexrand}
+. Idade: ${agerand}
 . Raça:
 . Função:
 . Nível:
-. Elemento:
+. Elemento: 
 . Fraqueza:
 . Vantagem:
   
