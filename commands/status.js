956fbb687394ecc17a0e956918da29a8b1492874
Glitch (hello-express) <none>
2020-05-26 13:54:09 +0000

    🚖⚾️ Checkpoint ./commands/status.js:16608023/581 ./watch.json:16608023/16

--- commands/status.js
+++ commands/status.js
@@ -2,14 +2,12 @@ const Discord = require('discord.js');
 
 exports.run = async (client, message, args) => {
 
-const content = [`
+const content = [''
               
                 TESTANDO A PORRA DO COMANDO EM JAVA!
-                🔍 Informações do usuário - ${message.author.tag}
-
-
-
-  `]
+                
+                
+  '']
 
 const member = message.mentions.users.first() || client.users.cache.get(args[0]) || message.member;
 
@@ -18,10 +16,11 @@ let avatar = message.author.displayAvatarURL({format: "png"});
 const embed = new Discord.MessageEmbed()
         
         .setAuthor(`🔍 Informações do usuário - ${message.author.tag}`)
+        
         .setTitle('Estes são os comandos disponiveis atualmente:')
         .setColor('RANDOM')
-        .setDescription(member)
-        .setThumbnail(avatar)  
+        .setDescription(content)
+        .setThumbnail(avatar)
         .setFooter('Esperamos aumentar a lista em breve! Obrigado pelo uso.')
         
 await message.channel.send(embed);
