7ee92300a1addce45250968954cd031582026f0b
Glitch (hello-express) <none>
2020-05-27 13:18:13 +0000

    🏓😻 Checkpoint ./commands/nivmas.js:16608023/1370 ./watch.json:16608023/6

--- commands/nivmas.js
+++ commands/nivmas.js
@@ -15,25 +15,30 @@ const number = [
                 ':zero:'
                ];
   
-var rand2 = mumber[Math.floor(Math.random() * number.length)];  
+var rand2 = number[Math.floor(Math.random() * number.length)];
   
+var rand1 = number[Math.floor(Math.random() * number.length)];
+   
 var list = [
-  'https://imgur.com/g54UpQP.gif',
-  'https://imgur.com/w06DjGv.gif',
-  'https://imgur.com/SZD4ZLG.gif',
-  'https://imgur.com/BNxLtAE.gif'
+  'https://imgur.com/ypsad6p.gif',
+  'https://imgur.com/LxbLDuV.gif',
+  'https://imgur.com/E68sOrQ.gif',
+  'https://imgur.com/r4YIriv.gif',
+  'https://imgur.com/6UCiQnB.gif',
+  'https://imgur.com/fjE6T1z.gif',
+  'https://imgur.com/1q2s4Is.gif'
    
 ];
 
-  message.delete().catch(O_o => {});
+message.delete().catch(O_o => {});
 
 var rand = list[Math.floor(Math.random() * list.length)];
   
 let avatar = message.author.displayAvatarURL({format: "png"});
   const embed = new Discord.MessageEmbed()
-        .setTitle('')
-        .setColor('RANDOM')
-        .setDescription(`${number}${number}`)
+        .setTitle('Seu Nível de Sensualidade Masculina é:')
+        .setColor('#34b1eb')
+        .setDescription(`===========> ${rand1}${rand2}% <===========`)
         .setImage(rand)
         .setTimestamp()
         .setThumbnail(avatar)
